4a3cc1082838050027dc7baff7408fcbb9623b2b
[Minh] <[minhhoang120220@gmail.com]>
2025-04-06 16:37:58 +0700

    add validation middleware

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -31,87 +31,99 @@ const pokemonTypes = [
 const fileData = JSON.parse(fs.readFileSync(dbFile, "utf8"));
 let pokemons = fileData.data;
 
-router.get("/", async (req, res, next) => {
-  try {
-    //define schema for validation
-    const requestQuerySchema = z.object({
-      search: z.string().optional(),
-      type: z.enum(pokemonTypes).optional(),
-      limit: z.coerce.number().int().min(1).max(100).default(10),
-      page: z.coerce.number().int().min(1).default(1),
-    });
+const validateRequestMiddleware =
+  (schema, requestParamPart) => (req, res, next) => {
+    // requestParamPart có thể là body/query/params
+    const result = requestQuerySchema.safeParse(req[requestParamPart]);
+    // req.query = {}
+    // sau khi parse, thì result.data = { limit: 10; page: 1}
 
-    // validate by defined schema + safeParse (not throw error by default --> be able to customize how to handle error)
-    const result = requestQuerySchema.safeParse(req.query);
-    // handle error
     if (!result.success) {
       return res.status(400).json({
-        message: "BAD REQUEST",
+        error: result.error.issues,
+        message: "Validation Failed",
       });
     }
 
-    let { search, type, limit, page } = result.data;
+    // requestParamPart = query
+    // req.query = result.data;
+    // req.query = { limit: 10; page: 1}
+    req[requestParamPart] = result.data;
+    next();
+  };
 
-    let filteredPokemons = pokemons;
-
-    if (search) {
-      search = search.toLowerCase();
-      filteredPokemons = pokemons.filter((p) =>
-        p.name.toLowerCase().includes(search.toLowerCase())
-      );
-    }
-
-    if (type) {
-      type = type.toLowerCase();
-      filteredPokemons = pokemons.filter((p) =>
-        p.types.some((t) => t.toLowerCase() === type)
-      );
-    }
-
-    const startIndex = (page - 1) * limit;
-    const endIndex = startIndex + limit;
-    const paginatedData = filteredPokemons.slice(startIndex, endIndex);
-
-    res.status(200).json({ data: paginatedData });
-  } catch (err) {
-    next(err);
-  }
+const requestQuerySchema = z.object({
+  search: z.string().optional(),
+  type: z.enum(pokemonTypes).optional(),
+  limit: z.coerce.number().int().min(1).max(100).default(10),
+  page: z.coerce.number().int().min(1).default(1),
 });
 
-router.get("/:id", async (req, res, next) => {
-  try {
-    // define schema for validation
-    const requestQuerySchema = z.object({
-      id: z.coerce.number().int(),
-    });
-    // validate by defined schema + safeParse (not throw error by default --> be able to customize how to handle error)
-    const result = requestQuerySchema.safeParse(req.params);
-    // handle error
-    if (!result.success) {
-      return res.status(400).json({
-        message: "BAD REQUEST",
-      });
-    }
-    // response logic
-    const { id } = result.data;
-    const currentIndex = pokemons.findIndex((p) => p.id === id);
-    const totalPokemons = pokemons.length;
-    const previousIndex = (currentIndex - 1 + totalPokemons) % totalPokemons;
-    const nextIndex = (currentIndex + 1) % totalPokemons;
-    const previousPokemon = pokemons[previousIndex];
-    const pokemon = pokemons[currentIndex];
-    const nextPokemon = pokemons[nextIndex];
-
-    if (!pokemon) {
-      return res.status(404).json({
-        message: "NOT FOUND",
-      });
-    }
-    res.status(200).json({ data: { pokemon, previousPokemon, nextPokemon } });
-  } catch (err) {
-    next(err);
-  }
+const getPokemonByIdSchema = z.object({
+  id: z.coerce.number().int(),
 });
+
+// middleware trong express là 1 function có 3 biến (req,res,) => {}
+router.get(
+  "/",
+  validateRequestMiddleware(requestQuerySchema, "query"),
+  async (req, res, next) => {
+    try {
+      let { search, type, limit, page } = req.query;
+
+      let filteredPokemons = pokemons;
+
+      if (search) {
+        search = search.toLowerCase();
+        filteredPokemons = pokemons.filter((p) =>
+          p.name.toLowerCase().includes(search.toLowerCase())
+        );
+      }
+
+      if (type) {
+        type = type.toLowerCase();
+        filteredPokemons = pokemons.filter((p) =>
+          p.types.some((t) => t.toLowerCase() === type)
+        );
+      }
+
+      const startIndex = (page - 1) * limit;
+      const endIndex = startIndex + limit;
+      const paginatedData = filteredPokemons.slice(startIndex, endIndex);
+
+      res.status(200).json({ data: paginatedData });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.get(
+  "/:id",
+  validateRequestMiddleware(getPokemonByIdSchema, "params"),
+  async (req, res, next) => {
+    try {
+      // response logic
+      const { id } = req.params;
+      const currentIndex = pokemons.findIndex((p) => p.id === id);
+      const totalPokemons = pokemons.length;
+      const previousIndex = (currentIndex - 1 + totalPokemons) % totalPokemons;
+      const nextIndex = (currentIndex + 1) % totalPokemons;
+      const previousPokemon = pokemons[previousIndex];
+      const pokemon = pokemons[currentIndex];
+      const nextPokemon = pokemons[nextIndex];
+
+      if (!pokemon) {
+        return res.status(404).json({
+          message: "NOT FOUND",
+        });
+      }
+      res.status(200).json({ data: { pokemon, previousPokemon, nextPokemon } });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
 
 router.post("/", async (req, res, next) => {
   try {
